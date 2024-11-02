9308d352e05f9abc7b58c5afdbfdf536d3991d2e
Paul Sheridan <paulsheridan@users.noreply.github.com>
2024-11-02 11:39:38 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
